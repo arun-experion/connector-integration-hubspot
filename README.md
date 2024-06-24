9b642cb9a9cdc7188462cce1e061d9df9be43229
arun-experion <arun.ks@experionglobal.com>
2024-06-24 10:53:37 +0530

    Unit test code added. Created by Cedric

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # HubSpot Integration
 
-#Notes
+
--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -1,5 +1,10 @@
 <?php
+namespace Tests;
 
+use Connector\Integrations\Hubspot\Integration;
+use Connector\Schema\IntegrationSchema;
+use Connector\Type\JsonSchemaFormats;
+use Connector\Type\JsonSchemaTypes;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -8,7 +13,114 @@ use PHPUnit\Framework\TestCase;
 final class IntegrationTest extends TestCase
 {
 
-    function testDiscover() {
-        // TODO
+    public array $oauthConfig = [];
+
+    protected function setUp(): void
+    {
+        $this->oauthConfig = [
+            'client_id'     => getenv('OAUTH_CLIENT_ID'),
+            'client_secret' => getenv('OAUTH_CLIENT_SECRET')
+        ];
     }
+
+    function testDiscoverReturnsJsonSchema() {
+        $integration = new Integration($this->oauthConfig);
+        $integration->setAuthorization(json_encode([
+            "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
+            "refreshToken" => getenv('OAUTH_REFRESH_TOKEN'),
+            "expires"      => (int) getenv('OAUTH_EXPIRES')]));
+
+        $schema = $integration->discover();
+        $this->assertInstanceOf(IntegrationSchema::class, $schema);
+        $this->assertJson($schema->json);
+        $this->assertArrayHasKey('$schema',$schema->schema);
+        $this->assertArrayHasKey('$id',$schema->schema);
+        $this->assertArrayHasKey('title',$schema->schema);
+        $this->assertArrayHasKey('type',$schema->schema);
+        $this->assertEquals('array',$schema->schema['type']);
+        $this->assertArrayHasKey('items',$schema->schema);
+    }
+
+    function testDiscoverReturnsCompanyDefinition() {
+        $integration = new Integration($this->oauthConfig);
+        $integration->setAuthorization(json_encode([
+                                                       "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
+                                                       "refreshToken" => getenv('OAUTH_REFRESH_TOKEN'),
+                                                       "expires"      => (int) getenv('OAUTH_EXPIRES')]));
+
+        $schema = $integration->discover();
+
+        // Not an exhaustive list of properties.
+
+        $this->assertTrue($schema->hasProperty('company','name'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('company','name')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('company','name')->format);
+
+        $this->assertTrue($schema->hasProperty('company','domain'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('company','domain')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('company','domain')->format);
+
+        $this->assertTrue($schema->hasProperty('company','city'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('company','city')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('company','city')->format);
+    }
+
+    function testDiscoverReturnsContactDefinition() {
+        $integration = new Integration($this->oauthConfig);
+        $integration->setAuthorization(json_encode([
+                                                       "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
+                                                       "refreshToken" => getenv('OAUTH_REFRESH_TOKEN'),
+                                                       "expires"      => (int) getenv('OAUTH_EXPIRES')]));
+
+        $schema = $integration->discover();
+
+        // Not an exhaustive list of properties.
+
+        $this->assertTrue($schema->hasProperty('contact','email'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('contact','email')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('contact','email')->format);
+
+        $this->assertTrue($schema->hasProperty('contact','firstname'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('contact','firstname')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('contact','firstname')->format);
+
+        $this->assertTrue($schema->hasProperty('contact','lastname'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('contact','lastname')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('contact','lastname')->format);
+
+        $this->assertTrue($schema->hasProperty('contact','phone'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('contact','phone')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('contact','phone')->format);
+
+        $this->assertTrue($schema->hasProperty('contact','jobtitle'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('contact','jobtitle')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('contact','jobtitle')->format);
+    }
+
+    function testDiscoverReturnsDealDefinition() {
+        $integration = new Integration($this->oauthConfig);
+        $integration->setAuthorization(json_encode([
+                                                       "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
+                                                       "refreshToken" => getenv('OAUTH_REFRESH_TOKEN'),
+                                                       "expires"      => (int) getenv('OAUTH_EXPIRES')]));
+
+        $schema = $integration->discover();
+
+        // Not an exhaustive list of properties.
+
+        $this->assertTrue($schema->hasProperty('deal','hs_acv'));
+        $this->assertEquals(JsonSchemaTypes::Number, $schema->getDataType('deal','hs_acv')->type);
+
+        $this->assertTrue($schema->hasProperty('deal','hs_arr'));
+        $this->assertEquals(JsonSchemaTypes::Number, $schema->getDataType('deal','hs_arr')->type);
+
+        $this->assertTrue($schema->hasProperty('deal','hs_closed_won_date'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('deal','hs_closed_won_date')->type);
+        $this->assertEquals(JsonSchemaFormats::DateTime, $schema->getDataType('deal','hs_closed_won_date')->format);
+
+        $this->assertTrue($schema->hasProperty('deal','dealname'));
+        $this->assertEquals(JsonSchemaTypes::String, $schema->getDataType('deal','dealname')->type);
+        $this->assertEquals(JsonSchemaFormats::None, $schema->getDataType('deal','dealname')->format);
+    }
+
 }
