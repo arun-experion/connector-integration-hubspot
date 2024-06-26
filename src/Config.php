bb46f33897bca418ba1f55c1acd19f3c9b630a5a
Treesa James <treesa.james@experionglobal.local>
2024-06-26 10:28:59 +0530

    Intial Commit for config

---
+++ src/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+class Config{
+    public const CLIENT_ID="bc1e39c3-95f0-4655-8160-df2f2233e296";
+    public const CLIENT_SECRET= "d8251725-d0bc-4423-afcb-fe1036b25244";
+
+    public const REDIRECT_URI="https://app.hubspot.com/oauth/authorize?client_id=bc1e39c3-95f0-4655-8160-df2f2233e296&redirect_uri=http://localhost&scope=crm.schemas.companies.write%20crm.schemas.quotes.read%20crm.schemas.contacts.write%20crm.objects.line_items.read%20crm.objects.carts.write%20crm.schemas.deals.read%20crm.objects.line_items.write%20crm.objects.carts.read%20crm.schemas.deals.write%20crm.schemas.line_items.read%20crm.pipelines.orders.write%20crm.dealsplits.read_write%20crm.pipelines.orders.read%20crm.objects.subscriptions.read%20crm.schemas.orders.write%20crm.import%20crm.schemas.subscriptions.read%20crm.schemas.orders.read%20crm.schemas.commercepayments.read%20crm.objects.orders.write%20oauth%20crm.objects.owners.read%20crm.objects.commercepayments.read%20crm.objects.orders.read%20crm.objects.invoices.read%20crm.schemas.invoices.read%20crm.objects.leads.read%20crm.objects.leads.write%20crm.export%20crm.objects.users.read%20crm.objects.partner-clients.read%20crm.objects.contacts.write%20crm.objects.users.write%20crm.objects.partner-clients.write%20crm.objects.partner-accounts.read%20crm.objects.marketing_events.read%20crm.objects.marketing_events.write%20crm.schemas.custom.read%20crm.objects.custom.read%20crm.objects.feedback_submissions.read%20crm.objects.custom.write%20crm.objects.companies.write%20crm.lists.write%20crm.objects.goals.read%20crm.objects.companies.read%20crm.lists.read%20crm.objects.deals.read%20crm.schemas.contacts.read%20crm.objects.deals.write%20crm.objects.quotes.write%20crm.schemas.carts.write%20crm.objects.contacts.read%20crm.schemas.companies.read%20crm.objects.quotes.read%20crm.schemas.carts.read";
+
+    public const AUTH_URI="https://app.hubspot.com/oauth/authorize";
+
+    public const TOKEN_URI= "https://api.hubapi.com/oauth/v1/token";
+
+    public const SCOPES= "";
+}
--- src/Integration.php
+++ src/Integration.php
@@ -3,19 +3,28 @@
 namespace Connector\Integrations\Hubspot;
 
 use Connector\Integrations\AbstractIntegration;
+use Connector\Integrations\Authorizations\OAuthInterface;
+use Connector\Integrations\Authorizations\OAuthTrait;
 use Connector\Integrations\Response;
 use Connector\Mapping;
 use Connector\Record\RecordKey;
 use Connector\Record\RecordLocator;
 use Connector\Schema\IntegrationSchema;
+use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 
-class Integration extends AbstractIntegration
+
+class Integration extends AbstractIntegration implements OAuthInterface
 {
+    use OAuthTrait;
 
     public function discover(): IntegrationSchema
     {
+
+        $schema = json_decode(file_get_contents(__DIR__."/../tests/testDiscover.json"),true);
+        return new IntegrationSchema($schema);
         // TODO: Implement discover() method.
+        // return new HubspotSchema();
     }
 
     public function extract(RecordLocator $recordLocator, Mapping $mapping, ?RecordKey $scope): Response
@@ -29,4 +38,22 @@ class Integration extends AbstractIntegration
 
     }
 
+    /**
+     * @throws \Connector\Exceptions\InvalidExecutionPlan
+     */
+    public function setAuthorization(string $authorization): void
+    {
+        $this->setOAuthCredentials($authorization);
+        // TODO: Implement setAuthorization() method.
+    }
+
+    public function getAuthorizationProvider(): AbstractProvider
+    {
+        // TODO: Implement getAuthorizationProvider() method.
+    }
+
+    public function getAuthorizedUserName(ResourceOwnerInterface $user): string
+    {
+        // TODO: Implement getAuthorizedUserName() method.
+    }
 }
