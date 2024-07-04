14c5f8ef4c76d8d359e1f7061d003e4adb69874f
Treesa James <treesa.james@experionglobal.local>
2024-07-04 13:56:46 +0530

    Unit Test for Authorization

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -35,7 +35,7 @@ final class IntegrationTest extends TestCase
         $this->assertArrayHasKey('results', $apiResponse);
     }
 
-    function testDiscoverReturnsJsonSchema(){
+    function testDiscoverReturnsJsonSchema() {
         $integration = new Integration($this->oauthConfig);
         $integration->setAuthorization(json_encode([
             "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
@@ -52,32 +52,31 @@ final class IntegrationTest extends TestCase
         $this->assertEquals('array',$schema->schema['type']);
         $this->assertArrayHasKey('items',$schema->schema);
     }
-
-    function testDiscoverReturnsCompanyDefinition(){
+    function testDiscoverReturnsCompanyDefinition() {
         $integration = new Integration($this->oauthConfig);
         $integration->setAuthorization(json_encode([
-            "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
-            "refreshToken" => getenv('OAUTH_REFRESH_TOKEN'),
-            "expires"      => (int) getenv('OAUTH_EXPIRES')]));
+                                                "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
+                                                "refreshToken" => getenv('OAUTH_REFRESH_TOKEN'),
+                                                "expires"      => (int) getenv('OAUTH_EXPIRES')]));
 
         $schema = $integration->discover();
 
         // Not an exhaustive list of properties.
 
-        $this->assertTrue($schema->hasProperty('company', 'name'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('company', 'name')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('company', 'name')->format);
+        $this->assertTrue($schema->hasProperty('company','name'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('company','name')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('company','name')->format);
 
-        $this->assertTrue($schema->hasProperty('company', 'domain'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('company', 'domain')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('company', 'domain')->format);
+        $this->assertTrue($schema->hasProperty('company','domain'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('company','domain')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('company','domain')->format);
 
         $this->assertTrue($schema->hasProperty('company', 'city'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('company', 'city')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('company', 'city')->format);
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('company','city')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('company','city')->format);
     }
 
-    function testDiscoverReturnsContactDefinition(){
+    function testDiscoverReturnsContactDefinition() {
         $integration = new Integration($this->oauthConfig);
         $integration->setAuthorization(json_encode([
             "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
@@ -88,28 +87,28 @@ final class IntegrationTest extends TestCase
 
         // Not an exhaustive list of properties.
 
-        $this->assertTrue($schema->hasProperty('contact', 'email'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact', 'email')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact', 'email')->format);
+        $this->assertTrue($schema->hasProperty('contact','email'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact','email')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact','email')->format);
 
-        $this->assertTrue($schema->hasProperty('contact', 'firstname'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact', 'firstname')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact', 'firstname')->format);
+        $this->assertTrue($schema->hasProperty('contact','firstname'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact','firstname')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact','firstname')->format);
 
-        $this->assertTrue($schema->hasProperty('contact', 'lastname'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact', 'lastname')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact', 'lastname')->format);
+        $this->assertTrue($schema->hasProperty('contact','lastname'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact','lastname')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact','lastname')->format);
 
-        $this->assertTrue($schema->hasProperty('contact', 'phone'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact', 'phone')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact', 'phone')->format);
+        $this->assertTrue($schema->hasProperty('contact','phone'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact','phone')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact','phone')->format);
 
-        $this->assertTrue($schema->hasProperty('contact', 'jobtitle'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact', 'jobtitle')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact', 'jobtitle')->format);
+        $this->assertTrue($schema->hasProperty('contact','jobtitle'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('contact','jobtitle')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('contact','jobtitle')->format);
     }
 
-    function testDiscoverReturnsDealDefinition(){
+    function testDiscoverReturnsDealDefinition() {
         $integration = new Integration($this->oauthConfig);
         $integration->setAuthorization(json_encode([
             "accessToken"  => getenv('OAUTH_ACCESS_TOKEN'),
@@ -120,19 +119,19 @@ final class IntegrationTest extends TestCase
 
         // Not an exhaustive list of properties.
 
-        $this->assertTrue($schema->hasProperty('deal', 'hs_acv'));
-        $this->assertEquals(JsonSchemaTypes::Number,$schema->getDataType('deal', 'hs_acv')->type);
+        $this->assertTrue($schema->hasProperty('deal','hs_acv'));
+        $this->assertEquals(JsonSchemaTypes::Number,$schema->getDataType('deal','hs_acv')->type);
 
-        $this->assertTrue($schema->hasProperty('deal', 'hs_arr'));
-        $this->assertEquals(JsonSchemaTypes::Number,$schema->getDataType('deal', 'hs_arr')->type);
+        $this->assertTrue($schema->hasProperty('deal','hs_arr'));
+        $this->assertEquals(JsonSchemaTypes::Number,$schema->getDataType('deal','hs_arr')->type);
 
-        $this->assertTrue($schema->hasProperty('deal', 'hs_closed_won_date'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('deal', 'hs_closed_won_date')->type);
-        $this->assertEquals(JsonSchemaFormats::DateTime,$schema->getDataType('deal', 'hs_closed_won_date')->format);
+        $this->assertTrue($schema->hasProperty('deal','hs_closed_won_date'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('deal','hs_closed_won_date')->type);
+        $this->assertEquals(JsonSchemaFormats::DateTime,$schema->getDataType('deal','hs_closed_won_date')->format);
 
-        $this->assertTrue($schema->hasProperty('deal', 'dealname'));
-        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('deal', 'dealname')->type);
-        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('deal', 'dealname')->format);
+        $this->assertTrue($schema->hasProperty('deal','dealname'));
+        $this->assertEquals(JsonSchemaTypes::String,$schema->getDataType('deal','dealname')->type);
+        $this->assertEquals(JsonSchemaFormats::None,$schema->getDataType('deal','dealname')->format);
     }
 
 }
