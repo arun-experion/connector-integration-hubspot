0cbd273eb7e820ff3e2f4fd81b889f676e06ea9a
Treesa James <treesa.james@experionglobal.local>
2024-07-04 13:19:22 +0530

    Unit Test for Authorization

--- src/Config.php
+++ src/Config.php
@@ -4,7 +4,6 @@ namespace Connector\Integrations\Hubspot;
 class Config{
     public const HUBSPOT_ACCESS_TOKEN='';
 
-
     // STANDARD_CRM_OBJECTS contains standard objects from HubSpot
     public const STANDARD_CRM_OBJECTS = ['contacts', 'companies', 'deals', 'tickets'];
 
