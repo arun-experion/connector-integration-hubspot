f54a53d503ee59f7887f1f0b4f00d773b974ca4e
arun-experion <arun.ks@experionglobal.com>
2024-07-09 10:13:30 +0530

    HubSpot token removed

--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 namespace Connector\Integrations\Hubspot;
 
 class Config{
-    public const HUBSPOT_ACCESS_TOKEN='pat-na1-a49bf2f9-4aea-48e8-9d58-a1ff8c0cabed';
+    public const HUBSPOT_ACCESS_TOKEN='';
 
     // STANDARD_CRM_OBJECTS contains standard objects from HubSpot
     public const STANDARD_CRM_OBJECTS = ['contacts', 'companies', 'deals', 'tickets'];
